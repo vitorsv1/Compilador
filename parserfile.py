f654199d3e261a9a0ad07be0000956a35c12898c
Vitor Satyro Vitturi <vitorsv1@al.insper.edu.br>
2020-09-09 17:37:39 -0300

    implementing V1.2

--- parserfile.py
+++ parserfile.py
@@ -6,55 +6,66 @@ class Parser:
 
     @staticmethod
     def parseExpression():
-        Parser.tokens.selectNext()
         result = Parser.parseTerm()
-        
-        while Parser.tokens.actual.type == "PLUS" or \
-            Parser.tokens.actual.type == "MINUS":
+        while Parser.tokens.actual.type == "PLUS" or Parser.tokens.actual.type == "MINUS":
             if Parser.tokens.actual.type == "PLUS":
                 Parser.tokens.selectNext()
-                if Parser.tokens.actual.type == "INT":
-                    result += Parser.parseTerm()
-                else:
-                    raise NameError(f"Type difference error, actual is {Parser.tokens.actual.type}, should be INT")
+                result += Parser.parseTerm()
             elif Parser.tokens.actual.type == "MINUS":
                 Parser.tokens.selectNext()
-                if Parser.tokens.actual.type == "INT":
-                    result -= Parser.parseTerm()
-                else:
-                    raise NameError(f"Type difference error, actual is {Parser.tokens.actual.type}, should be INT")
+                result -= Parser.parseTerm()
+            else:
+                raise NameError(f"Type difference error, actual is {Parser.tokens.actual.type}")
         return result
 
     @staticmethod
     def parseTerm():
-        result = 0
-        if Parser.tokens.actual.type == "INT":
-            result = Parser.tokens.actual.value
-            Parser.tokens.selectNext()
-            
-            while Parser.tokens.actual.type == "MULTI" or \
-                Parser.tokens.actual.type == "DIV":
-                if Parser.tokens.actual.type == "MULTI":
-                    Parser.tokens.selectNext()
-                    if Parser.tokens.actual.type == "INT":
-                        result = result * Parser.tokens.actual.value
-                    else:
-                        raise NameError(f"Type difference error, actual is {Parser.tokens.actual.type}, should be INT")
-                elif Parser.tokens.actual.type == "DIV":
-                    Parser.tokens.selectNext()
-                    if Parser.tokens.actual.type == "INT":
-                        result = result / Parser.tokens.actual.value
-                    else:
-                        raise NameError(f"Type difference error, actual is {Parser.tokens.actual.type}, should be INT")
+        result = Parser.parseFactor()
+        while Parser.tokens.actual.type == "MULTI" or Parser.tokens.actual.type == "DIV":
+            if Parser.tokens.actual.type == "MULTI":
                 Parser.tokens.selectNext()
-            return result
+                result = int(result * Parser.parseFactor())
+            elif Parser.tokens.actual.type == "DIV":
+                Parser.tokens.selectNext()
+                result = int(result / Parser.parseFactor())
+            else:
+                raise NameError(f"First type difference error, actual is {Parser.tokens.actual.type}")
+        return result
+
+    @staticmethod
+    def parseFactor():
+        res = 0
+        if Parser.tokens.actual.type == "INT":
+            res = int(Parser.tokens.actual.value)
+            Parser.tokens.selectNext()
+
+        elif Parser.tokens.actual.type == "OPEN_P":
+            Parser.tokens.selectNext()
+            res = Parser.parseExpression()
+
+            if Parser.tokens.actual.type == "CLOSE_P":
+                Parser.tokens.selectNext()
+            else:
+                raise NameError(f"Syntax error, ( open but not closed in position {Parser.tokens.position} with value {Parser.tokens.actual.value}")
+        
+        elif Parser.tokens.actual.type == "MINUS":
+            Parser.tokens.selectNext()
+            res -= Parser.parseFactor()
+
+        elif Parser.tokens.actual.type == "PLUS":
+            Parser.tokens.selectNext()
+            res += Parser.parseFactor()
+
         else:
-            raise NameError(f"First type difference error, actual is {Parser.tokens.actual.type}, should be INT")
+            raise NameError(f"Syntax error, Token Received was invalid, received type {Parser.tokens.actual.type}")
+        return res
+
 
     @staticmethod
     def run(code):
         code = PrePro.filter(code)
         Parser.tokens = Tokenizer(code)
+        Parser.tokens.selectNext()
         r = int(Parser.parseExpression())
         if Parser.tokens.actual.type == "EOF":
             return r
